0a7f52df42bc78bef0ea1b6433815aaddfb181ed
spencerWind <swind03@gmail.com>
2023-03-13 01:39:48 -0400

    added mvc functionality

--- server.py
+++ server.py
@@ -1,46 +1,5 @@
-from flask import Flask, render_template, redirect, request
-from user import User
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    users = User.get_all()
-    return render_template('home.html', users = users)
-
-@app.route('/user')
-def user():
-    return render_template('/user.html')
-
-@app.route('/user/create', methods=['POST'])
-def create_user():
-    data = {
-        "first_name": request.form['first_name'],
-        "last_name": request.form['last_name'],
-        "email": request.form['email'],
-    }
-    User.save(data)
-    return redirect('/')
-
-@app.route('/user/show/<int:user_id>')
-def show(user_id):
-    user = User.get_one(user_id)
-    return render_template("show_user.html", user = user)
-
-@app.route('/user/update/<int:user_id>')
-def show_update(user_id):
-    user = User.get_one(user_id)
-    return render_template("update_user.html", user = user)
-
-@app.route('/user/update', methods=["POST"])
-def update_user():
-    User.update(request.form)
-    return redirect('/')
-
-@app.route('/user/delete/<int:user_id>')
-def delete_user(user_id):
-    User.delete(user_id)
-    return redirect('/')
+from flask_app.controllers import users
+from flask_app import app
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
